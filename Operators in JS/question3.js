450637f4eb13fd69bffdd39c3b2892db0c7255b9
Basit-Bashir <wbhat56@gmail.com>
2023-11-08 15:44:50 +0530

    js operators

---
+++ Operators in JS/question3.js
@@ -0,0 +1,5 @@
+var number = 7;
+
+var result = number > 0 ? "positive" : number < 0 ? "negative" : "zero";
+
+console.log("The number is " + result + ".");
---
+++ Operators in JS/question4.js
@@ -0,0 +1,5 @@
+var age = 20;
+
+var result = age < 18 ? "You cannot vote" : "You can vote";
+
+console.log(result);
